6e6656d1ec7f6b74c570f4e9a0ccbb0b9c3ec12b
vijayakumar58 <vijayalingam58@gmail.com>
2022-11-15 12:38:15 +0530

    add js

--- script.js
+++ script.js
@@ -41,7 +41,17 @@
             }
            f3(); 
         }
-       
+       f2();
     }
-    
-    const independent= setTimeout(f1,1000);
+    f1();
+    // const independent1= setTimeout(f1,1000);
+    // const independent2= setTimeout(f2,2000);
+    // const independent3= setTimeout(f3,3000);
+    // const independent4= setTimeout(f4,4000);
+    // const independen5t= setTimeout(f5,5000);
+    // const independent6= setTimeout(f6,6000);
+    // const independent7= setTimeout(f7,7000);
+    // const independent8= setTimeout(f8,8000);
+    // const independent9= setTimeout(f9,9000);
+    // const independent10= setTimeout(f10,10000);
+    const independent11= setTimeout(f11,11000);
