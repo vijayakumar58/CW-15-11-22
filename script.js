76ed1f8fafc3e90515ca09d34cc8c2037b9e0916
vijayakumar58 <vijayalingam58@gmail.com>
2022-11-15 12:08:03 +0530

    add html

---
+++ script.js
@@ -0,0 +1,45 @@
+
+     f1 =()=>{
+        console.log("10")
+        f2=()=>{
+            console.log("9")
+            f3=()=>{
+                console.log("8")
+                f4=()=>{
+                    console.log("7")
+                    f5=()=>{
+                        console.log("6")
+                         f6=()=>{
+                            console.log("5")
+                                f7=()=>{
+                                console.log("4")
+                                 f8=()=>{
+                                    console.log("3")
+                                    f9=()=>{
+                                        console.log("2")
+                                          f10=()=>{
+                                            console.log("1")
+                                             f11=()=>{
+                                                console.log("independent day")
+                                            }
+                                            f11();
+                                        }
+                                        f10();
+                                    }
+                                   f9();
+                                }
+                               f8();
+                            }
+                           f7(); 
+                        }
+                       f6(); 
+                    }
+                    f5();
+                }
+               f4(); 
+            }
+           f3(); 
+        }
+        f2();
+    }
+    f1();
